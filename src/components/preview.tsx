1f4be9e05b26a9362932a9b1775d13d93aea0b7a
ckaylor33 <kaylor33@hotmail.co.uk>
2021-01-26 14:47:31 +0000

    error handling in th eiframe window, catching synchronous, asynchronous and bundling errors

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -3,6 +3,7 @@ import './preview.css'
 
 interface PreviewProps {
   code: string
+  err: string
 }
 
 const html = `
@@ -11,13 +12,20 @@ const html = `
   <body> 
     <div id='root'></div>
     <script>
+      const handleError = (err) => {
+        const root = document.querySelector('#root')
+          root.innerHTML = '<div style="color: red"><h4>Runtime Error</h4>'+ err + '</div>'
+          console.error(err)
+      }
+      window.addEventListener('error', (event) => {
+        event.preventDefault()
+        handleError(event.error)
+      })
       window.addEventListener('message', (event) => {
         try {
         eval(event.data)
         } catch(err) {
-          const root = document.querySelector('#root')
-          root.innerHTML = '<div style="color: red"><h4>Runtime Error</h4>'+ err + '</div>'
-          console.error(err);
+          handleError(err)
         }
       }, false)
     </script>
@@ -25,7 +33,7 @@ const html = `
 </html>
 `
 
-const Preview: React.FC<PreviewProps> = ({ code }) => {
+const Preview: React.FC<PreviewProps> = ({ code, err }) => {
   const iframe = useRef<any>()
 
   useEffect(() => {
@@ -34,6 +42,7 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
       iframe.current.contentWindow.postMessage(code, '*')
     }, 50)
   }, [code])
+
   return (
     <div className='preview-wrapper'>
       <iframe
@@ -42,6 +51,7 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
         sandbox='allow-scripts'
         srcDoc={html}
       />
+      {err && <div className='preview-error'>{err}</div>}
     </div>
   )
 }
